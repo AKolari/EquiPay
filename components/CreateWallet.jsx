fb28c982ea580dfa1d41923b58f2c718dd73915f
Gab Erves <gerves20@gmail.com>
2023-10-29 15:08:47 -0400

    some edits

--- components/CreateWallet.jsx
+++ components/CreateWallet.jsx
@@ -132,6 +132,12 @@ const CreateWallet = ({userId}) => {
               />
             </div>
           <div className="sm:col-span-2">
+          <label
+                htmlFor="large-input"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Select Currency*
+              </label>
             <select className="block p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={selectedCurrency} onChange={handleCurrencyChange}>
             <option value="USD">USD</option>
             <option value="EUR">EUR</option>
