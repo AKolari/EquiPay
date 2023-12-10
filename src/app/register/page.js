8a1449fdd42a2c5f29c8618543441167cfdefb97
Andi Kolari <AndiKolari627@gmail.com>
2023-12-10 14:27:32 -0500

    Fixing stuff

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,9 +1,6 @@
-//import Register from "@/Components/Register";
-function Page(){
-    return (
-        <></>
-      //<Register/>
-    )
+import Register from "../../../components/register";
+function Page() {
+  return <Register />;
 }
 
 export default Page;
