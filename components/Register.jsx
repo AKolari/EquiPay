d01199f350c2ea95a1a379305e211214d59b3512
Andi Kolari <AndiKolari627@gmail.com>
2023-12-10 14:40:17 -0500

    Fix

--- components/Register.jsx
+++ components/Register.jsx
@@ -1,3 +1,13 @@
+"use client";
+
+import React from "react";
+
+import "/src/app/globals.css";
+
+import { registerUser } from "../utils/dataQuery";
+import { useReducer } from "react";
+import { useRouter } from "next/navigation";
+
 const Register = () => {
   return <></>;
 };
